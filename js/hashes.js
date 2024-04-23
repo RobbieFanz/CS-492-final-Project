a843281f7e6c99d2b89c6381fa5e4603ef1ea5e3
RobbieFanz <fanzuttir@gmail.com>
2024-04-23 17:14:07 -0400

    refactored UI

--- js/hashes.js
+++ js/hashes.js
@@ -1,8 +1,19 @@
 function MD5(input){
     var start = performance.now()
-    var MD5 = new Hashes.MD5
+    var MD5 = new Hashes.MD5//error on this line
     var output = MD5.hex(input)
     var end = performance.now()
 
     return [output,end-start] 
 }
+
+
+
+const button = $('#submit')
+
+button.click(function() {
+    input = $('#input').val();
+    $('#MD5').text(`${input}`);
+    $('#SHA256').text(`${input}`);
+    $('#tiger').text(`${input}`);
+});
