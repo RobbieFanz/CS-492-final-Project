a00840139e8733585b6bdab196c37eb5f261d8ac
RobbieFanz <fanzuttir@gmail.com>
2024-04-23 17:40:18 -0400

    links to dependency in HTML & gives MD5 output

--- js/hashes.js
+++ js/hashes.js
@@ -1,10 +1,10 @@
 function MD5(input){
     var start = performance.now()
-    var MD5 = new Hashes.MD5//error on this line
+    var MD5 = new Hashes.MD5
     var output = MD5.hex(input)
     var end = performance.now()
 
-    return [output,end-start] 
+    return output
 }
 
 
@@ -13,7 +13,7 @@ const button = $('#submit')
 
 button.click(function() {
     input = $('#input').val();
-    $('#MD5').text(`${input}`);
+    $('#MD5').text(`${MD5(input)}`);
     $('#SHA256').text(`${input}`);
     $('#tiger').text(`${input}`);
 });
