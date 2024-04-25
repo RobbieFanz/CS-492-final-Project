25a1abedd00da407eec188da53ac422354e5e243
RickyMcBride <116308895+RickyMcBride@users.noreply.github.com>
2024-04-25 18:03:25 +0000

    added sha256/512 in js and html

--- js/hashes.js
+++ js/hashes.js
@@ -1,3 +1,5 @@
+//const Hashes = require("jshashes")
+
 function MD5(input){
     var start = performance.now()
     var MD5 = new Hashes.MD5
@@ -12,7 +14,25 @@ function SHA1(input){
     var SHA1 = new Hashes.SHA1
     var output = SHA1.hex(input)
     var end = performance.now()
-    return output 
+    return output
+
+
+}
+
+function SHA256(input){
+    var start = performance.now()
+    var SHA256 = new Hashes.SHA256
+    var output = SHA256.hex(input)
+    var end = performance.now()
+    return output
+}
+
+function SHA512(input){
+    var start = performance.now()
+    var SHA512 = new Hashes.SHA512
+    var output = SHA512.hex(input)
+    var end = performance.now()
+    return output
 }
 
 
@@ -22,5 +42,7 @@ button.click(function() {
     input = $('#input').val();
     $('#MD5').text(`${MD5(input)}`);
     $('#SHA1').text(`${SHA1(input)}`);
-    $('#SHA256').text(`${input}`);
+    $('#SHA256').text(`${SHA256(input)}`);
+    $('#SHA512').text(`${SHA512(input)}`);
+
 });
